2a3b2133090a13aa07cf5ff2983584a2559d3667
lighttttt <yxu70832@usc.edu>
2021-09-20 09:28:03 -0700

    Initial Commit

---
+++ counselor_assistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  counselor_assistant
+//
+//  Created by Yi Xu on 9/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
