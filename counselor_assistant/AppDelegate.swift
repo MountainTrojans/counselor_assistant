f4b8b71ea65ae5dea1c1637475dfddda369f0e52
lighttttt <yxu70832@usc.edu>
2021-09-23 11:50:07 -0700

    Firebase Auth

--- counselor_assistant/AppDelegate.swift
+++ counselor_assistant/AppDelegate.swift
@@ -7,16 +7,38 @@
 
 import UIKit
 import Firebase
+import FirebaseUI
+
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, FUIAuthDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        let authUI = FUIAuth.defaultAuthUI()
+        // You need to adopt a FUIAuthDelegate protocol to receive callback
+        authUI?.delegate = self
+        
+        let providers: [FUIAuthProvider] = [
+          FUIGoogleAuth(),
+        ]
+        authUI?.providers = providers
+        
+        
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+      if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+        return true
+      }
+      // other URL handling goes here.
+      return false
     }
 
     // MARK: UISceneSession Lifecycle
