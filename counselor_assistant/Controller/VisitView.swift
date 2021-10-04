b58b67c3182830dcc9b5bb97afb1313d286b8a76
Tyler Rosselli <trossell@usc.edu>
2021-10-03 23:59:41 -0700

    added the storyboard for visit

---
+++ counselor_assistant/Controller/VisitView.swift
@@ -0,0 +1,15 @@
+//
+//  VisitView.swift
+//  counselor_assistant
+//
+//  Created by Tyler Rosselli on 10/3/21.
+//
+
+import Foundation
+
+import UIKit
+
+class VisitViewController: UIViewController {
+    
+}
+
